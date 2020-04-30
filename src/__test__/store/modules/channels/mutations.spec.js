834dec3bb2cddd4f11d2de7c26dc415a187a50fd
ryota kobayashi <ryotakobayashi@koba.local>
2020-05-01 00:57:44 +0900

    modified mutations and state

--- src/__test__/store/modules/channels/mutations.spec.js
+++ src/__test__/store/modules/channels/mutations.spec.js
@@ -16,6 +16,17 @@ describe('store/modules/channels/mutations.jsのテスト', () => {
     };
   };
 
+  const createMockStateWithChannel = () => {
+    return {
+      channels: [
+        {
+          id: 'general',
+          messages: []
+        }
+      ]
+    };
+  };
+
   it('setChannelsメソッドのテスト', () => {
     const state = createMockStateWithoutChannels();
     const newChannels = ['new channel', 'new channel2'];
@@ -32,7 +43,7 @@ describe('store/modules/channels/mutations.jsのテスト', () => {
       const state = createMockStateWithLoading();
       const loadingType = 'channels';
 
-      mutations.setLoading(state, { loadingType, isLoading: true});
+      mutations.setLoading(state, { loadingType, isLoading: true });
       expect(state).toStrictEqual({
         loading: {
           channels: true,
@@ -40,7 +51,7 @@ describe('store/modules/channels/mutations.jsのテスト', () => {
         }
       });
 
-      mutations.setLoading(state, {loadingType, isLoading: false});
+      mutations.setLoading(state, { loadingType, isLoading: false });
       expect(state).toStrictEqual({
         loading: {
           channels: false,
@@ -49,11 +60,11 @@ describe('store/modules/channels/mutations.jsのテスト', () => {
       });
     });
 
-    it('messagesのフラグ動作確認', () => {
+    it('messages のフラグ動作確認', () => {
       const state = createMockStateWithLoading();
       const loadingType = 'messages';
 
-      mutations.setLoading(state, { loadingType, isLoading: true});
+      mutations.setLoading(state, { loadingType, isLoading: true });
       expect(state).toStrictEqual({
         loading: {
           channels: false,
@@ -61,13 +72,50 @@ describe('store/modules/channels/mutations.jsのテスト', () => {
         }
       });
 
-      mutations.setLoading(state, { loadingType, isLoading: false});
+      mutations.setLoading(state, { loadingType, isLoading: false });
       expect(state).toStrictEqual({
         loading: {
           channels: false,
           messages: false
         }
       });
+    });
+  });
+
+  it('setChannelMessagesメソッドのテスト', () => {
+    const state = createMockStateWithChannel();
+    const newMessages = ['new message1', 'new message2'];
+    const payload = {
+      channelId: state.channels[0].id,
+      messages: newMessages
+    };
+
+    mutations.setChannelMessages(state, payload);
+    expect(state).toStrictEqual({
+      channels: [
+        {
+          id: state.channels[0].id,
+          messages: [...newMessages]
+        }
+      ]
+    });
+  });
+
+  it('addMessageメソッドのテスト', () => {
+    const state = createMockStateWithChannel();
+    const payload = {
+      channelId: state.channels[0].id,
+      message: 'message',
+    };
+
+    mutations.addMessage(state, payload);
+    expect(state).toStrictEqual({
+      channels: [
+        {
+          id: state.channels[0].id,
+          messages: [payload.message]
+        }
+      ]
     });
   });
 });
